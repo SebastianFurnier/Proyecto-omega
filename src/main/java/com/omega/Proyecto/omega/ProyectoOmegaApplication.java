1f4dd51708053fa67287d83ad62395f21678fe37
Seba <sfurnier@hotmail.com>
2024-02-18 11:20:47 -0300

    Initial commit

---
+++ src/main/java/com/omega/Proyecto/omega/ProyectoOmegaApplication.java
@@ -0,0 +1,13 @@
+package com.omega.Proyecto.omega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoOmegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoOmegaApplication.class, args);
+	}
+
+}
