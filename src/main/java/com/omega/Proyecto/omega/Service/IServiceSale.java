4ca490e905df6ba8f588cb2525a755c345517494
Seba <sfurnier@hotmail.com>
2024-02-29 16:16:01 -0300

    Merge Main into this branch.

--- src/main/java/com/omega/Proyecto/omega/Service/IServiceSale.java
+++ src/main/java/com/omega/Proyecto/omega/Service/IServiceSale.java
@@ -10,6 +10,9 @@ public interface IServiceSale {
     public Sale createSale(Sale sale) throws ErrorDataException;
     public void deleteSale(Long id) throws ObjectNotFoundException;
     public Sale getSale(Long id) throws ObjectNotFoundException;
+
+    Sale getInactiveSale(Long id);
+
     public List<Sale> getAllSales();
     List<Sale> getAllInactiveSales();
 }
